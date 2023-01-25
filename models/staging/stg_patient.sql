a71e6ca5abe80bad0d3d052f4fd9b044d130974f
Nay Zaw Aung Win <nay_win@hakkoda.io>
2023-01-25 16:01:06 +0000

    Rearranged the order of the columns in stg_patient.sql so that it is the same order they appear in the raw table in snowflake

--- models/staging/stg_patient.sql
+++ models/staging/stg_patient.sql
@@ -17,23 +17,23 @@ patients as (
 
     select 
 
+        address as patient_address,
+        birthdate as patient_birthdate,
+        communication as patient_preferred_language,
+        deceaseddatetime as patient_deceased_datetime,
+        extension as patient_race_and_origin_info,
+        gender as patient_gender,
         id as patient_id,
         identifier as patient_id_and_other_ids,
-        name as patient_name,
-        address as patient_address,
-        telecom as patient_phone_number,
-        gender as patient_gender,
-        extension as patient_race_and_origin_info,
-        birthdate as patient_birthdate,
         maritalstatus as patient_marital_status,
-        communication as patient_preferred_language,
-        multiplebirthinteger as patient_birth_order,
-        multiplebirthboolean as patient_has_siblings,
-        deceaseddatetime as patient_deceased_datetime
-        --resourcetype as patient_or_other,
         --meta as patient_profile,
+        multiplebirthboolean as patient_has_siblings,
+        multiplebirthinteger as patient_birth_order,
+        name as patient_name,
+        --resourcetype as patient_or_other,
+        telecom as patient_phone_number
         --resource_text
-
+        
     from source
 
 )
