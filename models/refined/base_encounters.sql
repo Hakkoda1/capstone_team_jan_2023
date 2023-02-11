3ac15ae3b679976ba192bafaf16268883c50f82b
Nay Zaw Aung Win <nay_win@hakkoda.io>
2023-02-11 20:53:12 +0000

    added patient_id and encounter_class_code columns in base_encounters model

--- models/refined/base_encounters.sql
+++ models/refined/base_encounters.sql
@@ -15,7 +15,9 @@ base_encounters as (
         period:end::datetime as encounter_end_datetime,
         location[0]:location:display::varchar as encounter_location,
         reasoncode[0]:coding[0]:display::varchar as encounter_reason,
+        class:code::varchar as encounter_class_code,
         participant[0]:individual:display::varchar as provider_full_name,
+        split_part(subject:reference::varchar, ':', -1) as patient_id,
         subject:display::varchar as patient_full_name,
         hospitalization:dischargeDisposition:text::varchar as discharge_disposition
 
